17ed49556b9c645ae1b41430df991ba6b6f1dade
Palash <palashjain.ctinfotech@gmail.com>
2026-08-22 21:59:13 +0530

    chore: polyfill BsDropdownModule.forRoot for legacy ngx-intl-tel-input support

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+(BsDropdownModule as any).forRoot = () => ({ ngModule: BsDropdownModule, providers: [] as any[] });
 import { environment } from './environments/environment';
 
 if (environment.production) {
